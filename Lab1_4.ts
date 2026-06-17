fa0838cf54216a6549948616d327e81600e20a37
wannakarnsaelee123-ux <wannakarnsaelee123@gmail.com>
2026-06-17 16:59:23 +0700

    ส่งLab1_4

---
+++ Lab1_4.ts
@@ -0,0 +1,11 @@
+let GrossIncome:number = 50000;
+let Rate: number =7 ;
+let Paid: number =(GrossIncome*Rate)/100;
+let Net: number=(GrossIncome-Paid);
+
+console.log("---Tax calculation Report---");
+console.log("Gross Income:",GrossIncome);
+console.log("Tax Rate:",Rate,"%");
+console.log("Tax Paid:$",Paid);
+console.log("Net Income after Tax:$",Income);
+console.log("........................");
